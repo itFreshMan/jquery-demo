c1a4bb0d9e3e0a16b30550b69c5c488f0e055509
itFreshMan <jhuaishuang@126.com>
2015-09-14 17:08:02 +0800

    commit ss

--- resources/js/jquery/plugins/jquery.nav.js
+++ resources/js/jquery/plugins/jquery.nav.js
@@ -28,7 +28,7 @@
    //局部的 调用： $('.list').eq(0).nav('red'); //附带传递的参数
     $.fn.extend({
         'nav':function(color){
-            $(this).find('.nav').css({
+            this.find('.nav').css({
                 'list-style': 'none',
                 'margin':0,
                 'padding': 0,
@@ -36,14 +36,16 @@
                 'color':color
             });
 
-            $(this).find('.nav').parent().hover(
+           this.find('.nav').parent().hover(
                 function(){
                     $(this).find('.nav').slideDown('nomal');
                 },
                 function(){
                     $(this).find('.nav').stop().slideUp('nomal');
                 }
-            )
+            );
+
+            return this;
         }
     });
 
